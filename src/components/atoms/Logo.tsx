d7667c582d93d5eae3f738be3fece5986b657134
SooyoungKang <kang.sooyoung@hotmail.com>
2024-07-01 14:49:57 +0900

    Update Logo

--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -1,16 +1,15 @@
 import Image from "next/image";
 import Link from "next/link";
-import logo from "@/assets/images/logo.png";
+import logo from "@/assets/images/shanabunny.png";
 
 const Logo = () => (
   <Link className="inline-flex" href="/" aria-label="Cruip">
     <Image
-      className="max-w-none opacity-50 contrast-50 grayscale duration-75 hover:opacity-100 hover:contrast-100 hover:grayscale-0"
+      className="max-w-none opacity-80 contrast-80  duration-75 hover:opacity-100 hover:contrast-100"
       src={logo}
-      width={38}
-      height={38}
+      height={52}
       priority
-      alt="Stellar"
+      alt="shanabunny"
     />
   </Link>
 );
--- src/components/widgets/Header.tsx
+++ src/components/widgets/Header.tsx
@@ -21,7 +21,7 @@ export default function Header({ auth = false }: Props) {
   }, [auth, isSignedIn, setIsSignedIn]);
 
   return (
-    <header className="absolute w-full z-30">
+    <header className="absolute w-full z-30 mt-2">
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <div className="flex items-center justify-between h-16 md:h-20">
           {/* Site branding */}
